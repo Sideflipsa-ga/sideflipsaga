0b021e28834c55d44a9cd82ed65ad72b1a99d9ed
MrBrain295 <66077254+MrBrain295@users.noreply.github.com>
2020-12-02 14:34:39 -0600

    Delete

--- just my js.js
+++
@@ -1,6 +0,0 @@
-colors=["navy","thistle","gold","lightgreen","brown","lightblue","pink","orange","grey"]
-var timer=window.setInterval(changeColor, 10002);
-function changeColor(){
-	var n=Math.round(Math.random()*4);
-	document.body.style.backgroundColor=colors[n];
-	}
