312812f5b82b1c804f902edb2e6c7039427683b9
MrBrain295 <66077254+MrBrain295@users.noreply.github.com>
2020-12-05 16:35:49 -0600

    Update

--- most of my JS.js
+++ most of my JS.js
@@ -1,6 +1,6 @@
 colors=["navy","thistle","gold","lightgreen","brown","lightblue","pink","orange","grey","green","teal","red","purple","blue","yellow","hotpink"]
 var timer=window.setInterval(changeColor, 10002);
 function changeColor(){
-	var n=Math.round(Math.random()*4);
+	var n=Math.round(Math.random()*16);
 	document.body.style.backgroundColor=colors[n];
 	}
