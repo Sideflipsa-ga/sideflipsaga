6ac127456cc7a6ac2db1b0de6824f5d92c6a45a7
MrBrain295 <66077254+MrBrain295@users.noreply.github.com>
2020-12-12 16:12:39 -0600

    Update most of my JS.js

--- JS/most of my JS.js
+++ JS/most of my JS.js
@@ -1,6 +1,6 @@
-colors=["navy","thistle","gold","lightgreen","brown","lightblue","pink","orange","grey","green","teal","red","purple","blue","yellow","hotpink"]
-let timer=window.setInterval(changeColor, 10002);
+colors=["navy","thistle","gold","lightgreen","brown","lightblue","pink","orange","grey","green","teal","red","purple","blue","yellow","hotpink","peach","maroon","yellowgoldenrod","#d872c6","#cc9320","blanchedalmond","lightgoldenrodyellow","cornflowerblue","darkgoldenrod","chocolate"]
+let timer=window.setInterval(changeColor, 1002);
 function changeColor(){
-	let n=Math.round(Math.random()*16);
+	let n=Math.round(Math.random()*25);
 	document.body.style.backgroundColor=colors[n];
 	}
