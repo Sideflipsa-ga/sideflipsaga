c2feebf2b378a30f882eff9feccd156a2f4e7651
MrBrain295 <66077254+MrBrain295@users.noreply.github.com>
2020-12-24 14:15:36 -0600

    Update Most-of-my-JS.js

--- JS/Most-of-my-JS.js
+++ JS/Most-of-my-JS.js
@@ -1,6 +1,6 @@
-colors=["navy","thistle","gold","lightgreen","brown","lightblue","pink","orange","grey","green","teal","red","purple","blue","yellow","hotpink","peach","maroon","yellowgoldenrod","#d872c6","#cc9320","blanchedalmond","lightgoldenrodyellow","cornflowerblue","darkgoldenrod","chocolate"]
+colors=["navy","thistle","gold","lightgreen","brown","lightblue","pink","orange","grey","green","teal","red","purple","blue","yellow","hotpink","peach","maroon","yellowgoldenrod","#d872c6","#cc9320","blanchedalmond","lightgoldenrodyellow","cornflowerblue","darkgoldenrod","chocolate"."(0, 102, 85, 0.71)"]
 let timer=window.setInterval(changeColor, 1002);
 function changeColor(){
-	let n=Math.round(Math.random()*25);
+	let n=Math.round(Math.random()*26);
 	document.body.style.backgroundColor=colors[n];
 	}
