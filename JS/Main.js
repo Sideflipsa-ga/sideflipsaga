d9db425012b73567199f6d48e5ab6a364d2be8c6
MrBrain295 <66077254+MrBrain295@users.noreply.github.com>
2021-04-15 15:38:01 -0500

    Add ";"

--- JS/Main.js
+++ JS/Main.js
@@ -7,5 +7,5 @@ function changeColor(){
 
 
 
-alert("Hi from the SideFlipSa.ga devs!")
+alert("Hi from the SideFlipSa.ga devs!");
 console.log('%c Wow, I did not think anyone would come back here!', 'color: #00ffe5; font-size: 18px');
