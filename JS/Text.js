2f626b56b2264f1287abeba92034c36df8cba679
SideFli <73084992+SideFli@users.noreply.github.com>
2021-04-24 13:11:49 -0500

    Update Text.js

--- JS/Text.js
+++ JS/Text.js
@@ -1,3 +1,3 @@
 
-alert("Hi from the Sideflipsaga devs!(Side and Mrbrain");
+alert("Hi from the Sideflipsaga devs! -Side and Mrbrain");
 console.log('%c Let me speak to your MANAGER -Some Karen I met 4-23-21', 'color: #00ffe5; font-size: 18px');
