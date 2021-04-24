f502b1fd2792bdff28b36e9b901a9b43084bc968
SideFli <73084992+SideFli@users.noreply.github.com>
2021-04-24 13:04:49 -0500

    changed capitlition and added me and my partners name

--- JS/Text.js
+++ JS/Text.js
@@ -1,3 +1,3 @@
 
-alert("Hi from the SideFlipSaga devs!");
+alert("Hi from the Sideflipsaga devs!(Side and Mrbrain");
 console.log('%c Wow, I did not think anyone would come back here! -Side', 'color: #00ffe5; font-size: 18px');
