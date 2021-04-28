ea28687d0c2af5473268d4095b0d6f6367b94ba7
SideFli <73084992+SideFli@users.noreply.github.com>
2021-04-28 18:58:25 -0500

    Update Text.js

--- JS/Text.js
+++ JS/Text.js
@@ -1,3 +1,3 @@
 
 alert("Hi from the Sideflipsaga devs! -Side and Mrbrain");
-console.log('%c Let me speak to your MANAGER -Some Karen I met 4-23-21', 'color: #879E5C; font-size: 11px');
+console.log('%c lol.exe is a gud file -Side', 'color: #879E5C; font-size: 11px');
