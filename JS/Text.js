9448f988fd6228edae0ff5e45b77465104ef1bf8
MrBrain295 <66077254+MrBrain295@users.noreply.github.com>
2021-04-29 17:00:18 -0500

    why was that needed?

--- JS/Text.js
+++ JS/Text.js
@@ -1,3 +1,3 @@
 
-alert("Hi from the Sideflipsaga devs! -Side and Mrbrain");
+alert("Hi from the Sideflipsaga devs!");
 console.log('%c lol.exe is a gud file -Side', 'color: #879E5C; font-size: 11px');
