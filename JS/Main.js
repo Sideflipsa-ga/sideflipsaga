c10774b7b12d7c50a6cb36abef86481184ff0b3d
Gabe <66077254+MrBrain295@users.noreply.github.com>
2022-07-13 15:26:42 -0500

    Update Main.js

--- JS/Main.js
+++ JS/Main.js
@@ -1,6 +1,4 @@
-colors = function randomHsl() {
-  return "hsla(" + Math.random() * 360 + ", 100%, 50%, 1)";
-};
+const colors = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
 let timer = window.setInterval(changeColor, 1002);
 function changeColor() {
   document.body.style.backgroundColor = colors;
