5d5ffaeecce37e91a9441f30d6299cbf16340aaf
Gabe <66077254+MrBrain295@users.noreply.github.com>
2022-10-14 17:21:24 -0500

    Update Main.js

--- JS/Main.js
+++ JS/Main.js
@@ -1,11 +1,11 @@
 function random_bg_color() {
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
- console.log(bgColor);
-  
-    document.body.style.background = bgColor;
-    }
+  let x = Math.floor(Math.random() * 256);
+  let y = Math.floor(Math.random() * 256);
+  let z = Math.floor(Math.random() * 256);
+  let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+  console.log(bgColor);
+
+  document.body.style.background = bgColor;
+}
 
 random_bg_color();
