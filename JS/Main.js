f00f2a7473a304fc31bf8d25f0ade4d165e739e9
Gabe <66077254+MrBrain295@users.noreply.github.com>
2022-10-14 16:46:59 -0500

    Update Main.js

--- JS/Main.js
+++ JS/Main.js
@@ -1,9 +1,9 @@
 function random_rgba() {
     var o = Math.round, r = Math.random, s = 255;
     return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
-    document.body.style.backgroundColor = rgba;
 }
 
 for (var i=0; i<10; i++) {
-    console.log( random_rgba() ) 
+    console.log( random_rgba() )
+    document.body.style.backgroundColor = rgba;
 }
