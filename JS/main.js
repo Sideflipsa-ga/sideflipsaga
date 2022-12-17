3a975d6b53d2214c298c0d730e6d9bafa7b163a4
Gabe <66077254+MrBrain295@users.noreply.github.com>
2022-12-17 16:14:43 -0600

    optimize

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,13 @@
-function random_bg_color() {
-  let x = Math.floor(Math.random() * 256);
-  let y = Math.floor(Math.random() * 256);
-  let z = Math.floor(Math.random() * 256);
-  let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+function randomBgColor() {
+  // Use array destructuring to generate random colors
+  const [x, y, z] = [...Array(3)].map(() => Math.floor(Math.random() * 256));
+  const bgColor = `rgb(${x}, ${y}, ${z})`;
   console.log(bgColor);
 
   document.body.style.background = bgColor;
 }
 
-random_bg_color();
+randomBgColor();
 
 alert("Hi from the Sideflipsaga devs!");
 console.log("%c Im bored -Side", "color: #879E5C; font-size: 11px");
